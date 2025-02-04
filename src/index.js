45fdb2f6a960a5a8469fee187626b3a61391f072
josephkipkemoi <jkemboe@gmail.com>
2025-02-04 12:01:18 +0000

    Added Dynamic Imports in Code Splitting'

--- src/index.js
+++ src/index.js
@@ -5,28 +5,6 @@ import printMe from './print'
 
 import HeaderComponent from "./Components/Header"
 
-//  function component() {
-//    const element = document.createElement('div');
-//    element.classList.add("container")
-//   // Lodash, now imported by this script
-//    element.innerHTML = _.join(["Webpack", 'RiftKoders'], ' ');
-
-//    const myLogo = new Image()
-//    myLogo.src = Logo
-
-//    const btn = document.createElement("button")
-//    btn.innerText = "Click Me"
-//    btn.onclick = printMe
-
-//    element.appendChild(myLogo)
-
-//    element.appendChild(btn)
-
-//    return element;
-//  }
-
-//  document.body.appendChild(HeaderComponent());
-//  document.body.appendChild(component()); 
 
 async function getComponent() {
         const element = document.createElement('div')
